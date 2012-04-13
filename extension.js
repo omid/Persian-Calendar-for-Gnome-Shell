f09cb908cae7a98caf96e29a19a0946d96ed8c09
Omid Mottaghi <omidmr@gmail.com>
2012-04-13 11:55:35 +0430

    1- add events to calendar. 2- show holidays! 3- optimize code

--- extension.js
+++ extension.js
@@ -3,10 +3,18 @@ const Main = imports.ui.main;
 const PopupMenu = imports.ui.popupMenu;
 const PanelMenu = imports.ui.panelMenu;
 const MainLoop = imports.mainloop;
+const Lang = imports.lang;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
+
 const PersianDate = Me.imports.PersianDate;
+
+// events
+const persian = Me.imports.events.persian;
+const world = Me.imports.events.world;
+const iranSolar = Me.imports.events.iranSolar;
+//const iranLunar = Me.imports.events.iranLunar;
 
 function PopupMenuItem(label) {
     this._init(label);
@@ -19,7 +27,6 @@ PopupMenuItem.prototype = {
         PopupMenu.PopupBaseMenuItem.prototype._init.call(this);
 
         this.label = new St.Label({ text: text, style: 'direction: rtl;' });
-        //this.label.set_direction(St.TextDirection.RTL); // it should be RTL, but it is not RTL!!
         this.addActor(this.label);
     }
 };
@@ -35,55 +42,99 @@ PersianCalendar.prototype = {
         PanelMenu.Button.prototype._init.call(this, 0.0);
 
         this.label = new St.Label({ text: '', style: 'direction: rtl' });
-        //this.label.set_direction(St.TextDirection.RTL); // it should be RTL, but it is not RTL!!
         this.actor.add_actor(this.label);
-
-        this._date = new PopupMenuItem('');
-        this.menu.addMenuItem(this._date);
+        
+        this._events = Array();
+        this._events.push(new persian.persian);
+        this._events.push(new world.world);
+        this._events.push(new iranSolar.iranSolar);
+        //this._events.push(new iranLunar);
+        
+        this.today = '';
+        
+        this.popupMenuLabel = new PopupMenuItem('');
+        this.menu.addMenuItem(this.popupMenuLabel);
     },
 
     _updateDate: function() {
-		var _date = new Date();
-        _date = PersianDate.PersianDate.gregorianToPersian(_date.getFullYear(),_date.getMonth()+1,_date.getDate());
-        var _day = strFormat(_date[2] + '');
-        _date = strFormat(_date[2] + ' ' + PersianDate.PersianDate.p_month_names[_date[1]-1] + ' ' + _date[0]);
-
-		_indicator.label.set_text(_day);
-		_indicator._date.label.set_text(_date);
-		
-		return true;
-	}
+        this._isHoliday = false;
+        this._date = new Date();
+        this._day_in_year;
+        
+        // if it is friday
+        if(this._date.getDay() == 5) this._isHoliday = true;
+        
+        // convert to Persian
+        this._date = PersianDate.PersianDate.gregorianToPersian(this._date.getFullYear(), this._date.getMonth() + 1, this._date.getDate());
+        this._day_in_year = this._date[3];
+        
+        // if today is "today" just return, don't change anything!
+        if(this.today == this._day_in_year){
+            return true;
+        }
+        
+        // set today as "today"
+        this.today = this._day_in_year;
+        
+        // set indicator label and popupmenu
+        var _day = strFormat(this._date[2] + '');
+        this._date = strFormat(this._date[2] + ' ' + PersianDate.PersianDate.p_month_names[this._date[1]-1] + ' ' + this._date[0]);
+        
+        // get events of today
+        this._events.forEach(function(el){
+            el.events.forEach(this._checkEvent, this);
+        }, this);
+        
+        // is holiday?
+        if(this._isHoliday){
+            this.label.add_style_class_name("holiday");
+        } else {
+            this.label.remove_style_class_name("holiday");
+        }
+        
+        this.label.set_text(_day);
+        this.popupMenuLabel.label.set_text(this._date);
+        
+        return true;
+    },
+    
+    _checkEvent: function(el) {
+        if(this._day_in_year == el[0]){
+            this._date = this._date + "\n" + strFormat(el[1]);
+            this._isHoliday = this._isHoliday || el[2];
+        }
+    }
 };
 
 function strFormat(str, convert_numbers) {
-	if(!convert_numbers){
-		convert_numbers = true;
-	}
-	
-	var enums   = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-	var pnums   = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-	
-	var ncodes = ['\u06F0', '\u06F1', '\u06F2', '\u06F3', '\u06F4',
-	              '\u06F5', '\u06F6', '\u06F7', '\u06F8', '\u06F9'];
-				  
-	var chars = ['آ', 'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د',
-				 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع',
-				 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و', 'ه', 'ی'];
-	
-	var ccodes = ['\u0622', '\u0627', '\u0628', '\u067E', '\u062A',
-		 		  '\u062B', '\u062C', '\u0686', '\u062D', '\u062E',
-				  '\u062F', '\u0630', '\u0631', '\u0632', '\u0698',
-				  '\u0633', '\u0634', '\u0635', '\u0636', '\u0637',
-				  '\u0638', '\u0639', '\u063A', '\u0641', '\u0642',
-				  '\u06A9', '\u06AF', '\u0644', '\u0645', '\u0646',
-				  '\u0648', '\u0647', '\u0649'];
-				  
-	if(convert_numbers){
-		str = str_replace(enums, ncodes, str);
-		str = str_replace(pnums, ncodes, str);
-	}
-	
-	return str_replace(chars, ccodes, str);
+    if(!convert_numbers){
+        convert_numbers = true;
+    }
+    
+    var enums   = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var pnums   = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+    
+    var ncodes = ['\u06F0', '\u06F1', '\u06F2', '\u06F3', '\u06F4',
+                  '\u06F5', '\u06F6', '\u06F7', '\u06F8', '\u06F9'];
+                  
+    var chars = ['آ', 'ا', 'ب', 'پ', 'ت', 'ث', 'ج', 'چ', 'ح', 'خ', 'د',
+                 'ذ', 'ر', 'ز', 'ژ', 'س', 'ش', 'ص', 'ض', 'ط', 'ظ', 'ع',
+                 'غ', 'ف', 'ق', 'ک', 'گ', 'ل', 'م', 'ن', 'و', 'ه', 'ی'];
+    
+    var ccodes = ['\u0622', '\u0627', '\u0628', '\u067E', '\u062A',
+                   '\u062B', '\u062C', '\u0686', '\u062D', '\u062E',
+                  '\u062F', '\u0630', '\u0631', '\u0632', '\u0698',
+                  '\u0633', '\u0634', '\u0635', '\u0636', '\u0637',
+                  '\u0638', '\u0639', '\u063A', '\u0641', '\u0642',
+                  '\u06A9', '\u06AF', '\u0644', '\u0645', '\u0646',
+                  '\u0648', '\u0647', '\u06CC'];
+                  
+    if(convert_numbers){
+        str = str_replace(enums, ncodes, str);
+        str = str_replace(pnums, ncodes, str);
+    }
+    
+    return str_replace(chars, ccodes, str);
 }
 
 // to make it compatible with gnome-shell 3.3, I include this class to this file!
@@ -131,7 +182,7 @@ function enable() {
   _indicator = new PersianCalendar;
   Main.panel.addToStatusArea('persian_calendar', _indicator);
   _indicator._updateDate();
-  _timer = MainLoop.timeout_add(300000, _indicator._updateDate);
+  _timer = MainLoop.timeout_add(300000, Lang.bind(_indicator, _indicator._updateDate));
 }
 
 function disable() {
