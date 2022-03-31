345646d1024cd00c3d881afb5f3b78c6000f5628
Omid Rad <omidmr@gmail.com>
2022-03-31 19:33:19 +0200

    fix n__

--- PersianCalendar@oxygenws.com/utils/gettext.js
+++ PersianCalendar@oxygenws.com/utils/gettext.js
@@ -10,9 +10,9 @@ function __(str) {
     return new_str;
 }
 
-function n__(str) {
+function n__(msgid1, msgid2, n) {
     Gettext.setlocale(Gettext.LocaleCategory.ALL, Settings.get_string('language'));
-    let new_str = Gettext.dngettext(Me.metadata['gettext-domain'], str);
+    let new_str = Gettext.dngettext(Me.metadata['gettext-domain'], msgid1, msgid2, n);
     Gettext.setlocale(Gettext.LocaleCategory.ALL, '');
     return new_str;
 }
