4100beddce56ad8beeeb8b28d9b3a735400a7a2c
eshagh79 <eshagh094@gmail.com>
2022-04-02 15:32:15 +0430

    some change

--- PersianCalendar@oxygenws.com/Events.js
+++ PersianCalendar@oxygenws.com/Events.js
@@ -78,7 +78,7 @@ const Events = class {
         // if event is available, set event
         // and if it is holiday, set today as holiday!
         if (el.events[this._today[type][1]][this._today[type][2]]) {
-            this._events += `\n 🞄  ${str.wordWrap(el.events[this._today[type][1]][this._today[type][2]][0], 27)}`;
+            this._events += `\n⚫︎ ${str.wordWrap(el.events[this._today[type][1]][this._today[type][2]][0], 27)}`;
             this._isHoliday = this._isHoliday || el.events[this._today[type][1]][this._today[type][2]][1];
         }
     }
